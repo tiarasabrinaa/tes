f04bc90b27a3da07c549fa1138b1ab5243abc8ba
optiguard24 <138378589+tiarasabrinaa@users.noreply.github.com>
2024-09-15 12:57:15 +0700

    "cors lagi"

--- dist/src/middlewares/not-found.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function notFound(req, res, next) {
-    res.status(404);
-    const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
-    next(error);
-}
-exports.default = notFound;
-//# sourceMappingURL=not-found.js.map
